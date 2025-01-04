41c548073fc6d1dfea6b4d7e349bc608b6c6d3aa
Shiamak <shiamakdas@outlook.com>
2025-01-04 17:43:17 -0500

    Change star drawn based on star object

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+from star import *
 
 pygame.init()
 
@@ -8,12 +9,17 @@ SCREEN_HEIGHT = 600
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Solar System")
 
+star = Star("Gold")
+
 run = True
 
 while run:
     screen.fill((0, 0, 0))
     pygame.draw.circle(
-        screen, pygame.Color("Gold"), (SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2), 100
+        screen,
+        pygame.Color(star.color),
+        (SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2),
+        50 * star.mass,
     )
     pygame.display.flip()
 
