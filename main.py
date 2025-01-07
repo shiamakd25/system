e2ddea09a439910c5ed8b69a4e9934e25735092e
Shiamak <shiamakdas@outlook.com>
2025-01-06 20:23:25 -0500

    Display control panel in window

--- main.py
+++ main.py
@@ -7,11 +7,13 @@ SCREEN_INFO = pygame.display.Info()
 SCREEN_WIDTH = SCREEN_INFO.current_w
 SCREEN_HEIGHT = SCREEN_INFO.current_h - 96
 
-CONTROL_PANEL_WIDTH = 0.2 * SCREEN_WIDTH
+CONTROL_PANEL_WIDTH = 0.25 * SCREEN_WIDTH
 CONTROL_PANEL_HEIGHT = SCREEN_HEIGHT
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Solar System")
+
+control_panel = pygame.Surface((CONTROL_PANEL_WIDTH, CONTROL_PANEL_HEIGHT))
 
 star = Star("Gold")
 
@@ -19,6 +21,8 @@ run = True
 
 while run:
     screen.fill((0, 0, 0))
+    control_panel.fill((100, 100, 100))
+    screen.blit(control_panel, (SCREEN_WIDTH - CONTROL_PANEL_WIDTH, 0))
     pygame.draw.circle(
         screen,
         pygame.Color(star.color),
