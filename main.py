d38f33005b157213014b4365d520f5d7f7c3e959
Shiamak <shiamakdas@outlook.com>
2025-01-11 13:34:31 -0500

    Increase readability

--- main.py
+++ main.py
@@ -3,16 +3,16 @@ from globals import *
 from star import *
 from planet import *
 from ui import *
-import math
 
 pg.init()
 
-
+# Set up screen
 screen = pg.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pg.display.set_caption("Solar System")
 
 control_panel = pg.Surface((CONTROL_PANEL_WIDTH, CONTROL_PANEL_HEIGHT))
 
+# Control Panel elements
 mass_slider = Slider(
     (SCREEN_WIDTH - (CONTROL_PANEL_WIDTH / 2), 100),
     SLIDER_WIDTH,
@@ -49,49 +49,66 @@ p_dist_slider = Slider(
     "Distance from Star",
 )
 
+
+# Objects
 star_slider_dict = {"mass": mass_slider, "brightness": brightness_slider}
 star = Star((255, 228, 0), star_slider_dict)
 
 planet_slider_dict = {"mass": p_mass_slider, "distance": p_dist_slider}
 planet = Planet((255, 0, 225), planet_slider_dict)
 
+# Main Loop
 run = True
 
 while run:
-    screen.fill((0, 0, 0))
+    screen.fill((0, 0, 0))  # Fill in screen
+
+    # Draw Starlight
     pg.draw.circle(
         screen,
         pg.Color(tuple(val * star.brightness / 350 for val in star.color)),
         (DISPLAY_WIDTH / 2, DISPLAY_HEIGHT / 2),
         star.display_mass + 1.4 * star.brightness + 10,
     )
+
+    # Draw Star
     pg.draw.circle(
         screen,
         pg.Color(star.color),
         (DISPLAY_WIDTH / 2, DISPLAY_HEIGHT / 2),
         star.display_mass,
     )
+
+    # Draw Planet
     pg.draw.circle(
         screen,
         pg.Color(planet.color),
         (DISPLAY_WIDTH / 2 + planet.distance, DISPLAY_HEIGHT / 2 + planet.distance),
         planet.display_mass,
     )
+
+    # Add Control Panel
     control_panel.fill((100, 100, 100))
     screen.blit(control_panel, (SCREEN_WIDTH - CONTROL_PANEL_WIDTH, 0))
+
+    # Add Control Panel elements
     mass_slider.draw(screen)
     brightness_slider.draw(screen)
     p_mass_slider.draw(screen)
     p_dist_slider.draw(screen)
 
+    # Update object attributes
     star.update_attr(star_slider_dict)
     planet.update_attr(planet_slider_dict)
 
+    # Check events
     for event in pg.event.get():
+        # Update sliders
         mass_slider.update_value(event)
         brightness_slider.update_value(event)
         p_mass_slider.update_value(event)
         p_dist_slider.update_value(event)
+        # Close Window
         if event.type == pg.QUIT:
             run = False
 
