2dc2de764312da92eefe71a6780fbae2e9f3808f
Shiamak <shiamakdas@outlook.com>
2025-01-11 19:40:12 -0500

    Code readability

--- planet.py
+++ planet.py
@@ -1,14 +1,15 @@
 import math
+from ui import *
 
 
 class Planet:
-    def __init__(self, color, slider_dict):
+    def __init__(self, color: tuple, slider_dict: dict[str, Slider]):
         self.mass = slider_dict["mass"].initial_val
         self.display_mass = (8 * math.sqrt(3 * self.mass)) / 3 + 40
         self.distance = slider_dict["distance"].initial_val
         self.color = color
 
-    def update_attr(self, slider_dict):
+    def update_attr(self, slider_dict: dict[str, Slider]):
         self.mass = slider_dict["mass"].value
         self.display_mass = (8 * math.sqrt(3 * self.mass)) / 3 + 40
         self.distance = slider_dict["distance"].value
--- star.py
+++ star.py
@@ -1,15 +1,16 @@
 import math
+from ui import *
 
 
 class Star:
-    def __init__(self, color, slider_dict):
+    def __init__(self, color: tuple, slider_dict: dict[str, Slider]):
         self.mass = slider_dict["mass"].initial_val
         self.brightness = slider_dict["brightness"].initial_val
         self.display_mass = (8 * math.sqrt(3 * self.mass)) / 3 + 40
         self.display_brightness = self.display_mass + 1.4 * self.brightness + 10
         self.color = color
 
-    def update_attr(self, slider_dict):
+    def update_attr(self, slider_dict: dict[str, Slider]):
         self.mass = slider_dict["mass"].value
         self.display_mass = (8 * math.sqrt(3 * self.mass)) / 3 + 40
         self.brightness = slider_dict["brightness"].value
